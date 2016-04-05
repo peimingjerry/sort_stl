a376fa001fbd8e92f3845519d91f12afc15b9262
peimingjerry <zhoupeiming@qq.com>
2016-04-05 15:19:04 +0800

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <vector>
+using std::vector;
+
+#include <algorithm>
+using std::iterator_traits;
+
+#include <assert.h>
+
+#include "sort_algo.h"
+
+#include "data.h"
